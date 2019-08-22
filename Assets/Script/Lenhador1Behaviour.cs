3a641ccea9878bd2735176b556eb700cb35c5dd0
gustavosalm <gustavosalmeida9821@gmail.com>
2019-08-22 14:03:14 -0300

    lenhador

---
+++ Assets/Script/Lenhador1Behaviour.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lenhador1Behaviour : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    public float vida;
+    public float dano;
+    private bool andar;
+    private string nome;
+    void Start()
+    {
+        andar = true;
+    }
+
+    void Update()
+    {
+        print(nome);
+        if(GameObject.Find(nome) == null)
+        {
+            print("matou");
+            CancelInvoke("Atacar");
+            andar = true;
+        }
+        if (andar)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        nome = collision.name;
+        InvokeRepeating("Atacar", Time.time, 0.5f);
+        andar = false;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        print("matou");
+        CancelInvoke("Atacar");
+        andar = true;
+    }
+    void Atacar()
+    {
+        print(dano);
+    }
+}
