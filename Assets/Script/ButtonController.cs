003ce1956d5a83e5426ecd60b87a88eb982b47f4
Luiz-Edu0202 <Brayner.luizeduardo@gmail.com>
2019-08-28 12:02:08 -0300

    commite merge

---
+++ Assets/Script/ButtonController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    public GameObject triangulo;
+    public GameObject hexagono;
+    private Vector2 posicao;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    public void SpawnarTriangulo()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            posicao = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Instantiate(triangulo, posicao, Quaternion.identity);
+        }
+    }
+    public void SpawnarHexagono()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            posicao = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Instantiate(hexagono, posicao, Quaternion.identity);
+        }
+    }
+}
