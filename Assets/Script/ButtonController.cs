3cbbe5d881db4fe484801f47f7a10e2fb0a4a43c
Luiz-Edu0202 <Brayner.luizeduardo@gmail.com>
2019-08-29 11:49:05 -0300

    seletor de planta pronto

--- Assets/Script/ButtonController.cs
+++ Assets/Script/ButtonController.cs
@@ -7,6 +7,8 @@ public class ButtonController : MonoBehaviour
     public GameObject triangulo;
     public GameObject hexagono;
     private Vector2 posicao;
+    bool spawnTri;
+    bool spawnHex;
     void Start()
     {
         
@@ -14,22 +16,32 @@ public class ButtonController : MonoBehaviour
 
     void Update()
     {
-        
+        if(spawnTri)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                posicao = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Instantiate(triangulo, posicao, Quaternion.identity);
+                spawnTri = false;
+            }
+        }
+        if (spawnHex)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                posicao = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Instantiate(hexagono, posicao, Quaternion.identity);
+                spawnHex = false;
+            }
+        }
+
     }
     public void SpawnarTriangulo()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            posicao = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Instantiate(triangulo, posicao, Quaternion.identity);
-        }
+        spawnTri = true;        
     }
     public void SpawnarHexagono()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            posicao = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Instantiate(hexagono, posicao, Quaternion.identity);
-        }
+        spawnHex = true; 
     }
 }
