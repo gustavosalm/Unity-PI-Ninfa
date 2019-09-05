981f72662f94a4f7eeb4d573a39067e8f8707dec
tucoff <baracho.arturraffael@gmail.com>
2019-09-05 10:18:52 -0300

    boss ale e barra d spawn spawnando

--- Assets/Script/SpawnControl.cs
+++ Assets/Script/SpawnControl.cs
@@ -6,14 +6,34 @@ public class SpawnControl : MonoBehaviour
 {
     public GameObject enemy1;
     public Vector2 pos;
+
+    public Dictionary<towersBehaviour.Def, GameObject> dicio;
+    public GameObject[] spawns = new GameObject[3];
     void Start()
     {
         InvokeRepeating("SpawnEnemy", 0, 1);
+
+        dicio = new Dictionary<towersBehaviour.Def, GameObject>()
+        {
+            {towersBehaviour.Def.CACTO,  spawns[0]},
+            {towersBehaviour.Def.CARNIVORA, spawns[1]},
+            {towersBehaviour.Def.DEFESA, spawns[2]}
+        };
+        
     }
 
     void Update()
     {
-        
+        if(towersBehaviour.selecionado != towersBehaviour.Def.NONE)
+        {
+            if(Input.GetButtonDown("Fire1"))
+            {
+                pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Instantiate(dicio[towersBehaviour.selecionado], pos, Quaternion.identity);
+                towersBehaviour.selecionado = towersBehaviour.Def.NONE;
+            }            
+        }
+
     }
     void SpawnEnemy()
     {
---
+++ Assets/Script/towersBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class towersBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public enum Def
+    {
+        NONE = 0,
+        CACTO = 5,
+        CARNIVORA = 15,
+        DEFESA = 25
+    }
+    public Def cards;
+    public static Def selecionado;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SelecionarPlanta(Button planta)
+    {
+        selecionado = planta.GetComponent<towersBehaviour>().cards;
+    }
+
+}
