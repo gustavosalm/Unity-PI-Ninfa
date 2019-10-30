8bfdcc57c4d428bf20c1f1379f589f1093980b7e
Leonardo Viana <leonardoviana.nave@gmail.com>
2019-10-30 11:42:24 -0300

    Menu pronto dps da merda q gustavo fez

---
+++ Assets/Script/MenuController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("vazou");
+        Application.Quit();
+    }
+}
